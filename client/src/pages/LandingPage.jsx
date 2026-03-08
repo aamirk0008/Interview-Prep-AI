b4d9cdb5885cf4e5e1fd5e8a4bb35a2e2decf847
aamirk0008 <113338901+aamirk0008@users.noreply.github.com>
2026-03-08 16:35:16 +0530

    Landing page ui

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { APP_FEATURES } from "../utils/data";
 import { useNavigate } from 'react-router-dom';
 import { LuSparkles } from "react-icons/lu";
+import  heroImg  from "../assets/prep.png";
 
 const LandingPage = () => {
   const navigate = useNavigate();
@@ -14,7 +15,7 @@ const LandingPage = () => {
   return (
     <>
     <div className='w-full min-h-full bg-[#FFFCEF]'>
-      <div className='w-125 h-125 bg-amber-200/20 blur-[65px] absolute top-0 left-0'/>
+      <div className='w-100 h-125 bg-amber-200/20 blur-[65px] absolute top-0 left-0'/>
         <div className='container mx-auto px-4 pt-6 pb-50 relative z-10'>
           <header className='flex items-center justify-between mb-16'>
             <div className='text-xl text-black font-bold'>
@@ -48,6 +49,44 @@ const LandingPage = () => {
           </div>
         </div>
       
+    </div>
+
+    <div className='w-full min-h-full relative z-10 mb-56'>
+      <div>
+        <section className='flex items-center justify-center -mt-36'>
+          <img src={heroImg} alt="" className='w-[80vw] rounded-lg'/>
+        </section>
+      </div>
+
+      <div className='w-full min-h-full bg-[#FFFCEF] mt-10'>
+        <div className='container mx-auto px-4 pt-10 pb-20'>
+          <section className='mt-5'>
+            <h2 className='text-2xl font-medium text-center mb-12'>Features that makes you shine.</h2>
+
+            <div className='flex flex-col items-start gap-8'>
+              {/* First three Cards */}
+              <div className='grid grid-cols-1 md:grid-cols-3 gap-8 w-full'> 
+                {APP_FEATURES.slice(0, 3).map((feature, index) => (
+                  <div key={index} className='bg-[#FFFEF8] p-6 rounded-xl shadow-xs shadow-amber-100 border border-amber-100 hover:shadow-lg transition-shadow cursor-pointer'>
+                    <h3 className='text-base font-semibold mb-3'>{feature.title}</h3> 
+                    <p className='text-gray-600'>{feature.description}</p>
+                  </div>
+                ))}
+              </div>
+
+              {/* Last two Cards */}
+              <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+                {APP_FEATURES.slice(3).map((feature, index) => (
+                  <div key={index} className='bg-[#FFFEF8] p-6 rounded-xl shadow-xs shadow-amber-100 border border-amber-100 hover:shadow-lg transition-shadow cursor-pointer'>
+                    <h3 className='text-base font-semibold mb-3'>{feature.title}</h3>
+                    <p className='text-gray-600'>{feature.description}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
     </>
   )
